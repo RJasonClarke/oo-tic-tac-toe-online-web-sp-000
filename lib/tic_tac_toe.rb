845eb4d598e27aaf46a3b1aec84b2fff01be44c8
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-30 00:43:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,17 @@ class TicTacToe
     def valid_move?
       index.between?(0,8) && !position_taken?(index)
     end
+
+    def turn
+      puts "Enter 1 - 9"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+      if valid_move?(index)
+        move(index, current_player)
+        display_board
+      else
+        turn
+      end
+    end
+    end
 end
